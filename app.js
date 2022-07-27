bc35bbe8f3e37de9b1224349db5ef23af387a1cc
AlleyChang <yifanchang0602@gmail.com>
2022-07-27 17:28:57 +0800

    feat: add update todo function

--- app.js
+++ app.js
@@ -40,11 +40,29 @@ app.post('/todos', (req, res) => {
     .catch(error => console.log(error))
 })
 
-app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id', (req, res) => {   //get: detail頁面
   const id = req.params.id
   return Todo.findById(id)
     .lean()
     .then((todo) => res.render('detail', { todo } ))
+    .catch(error => console.log(error))
+})
+
+app.get('/todos/:id/edit', (req, res) => {   //get: edit頁面
+  const id = req.params.id
+  return Todo.findById(id)
+    .lean()
+    .then((todo) => res.render('edit', { todo }))
+    .catch(error => console.log(error))
+})
+app.post('/todos/:id/edit', (req, res) => {  //post: edit頁面進行修改資料
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.findById(id)
+    .then((todo) => {
+      todo.name = name
+      return todo.save()})
+    .then(() => res.redirect(`/todos/${id}`))
     .catch(error => console.log(error))
 })
 
