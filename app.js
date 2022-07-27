f2b200a914fc0901637253216dd245f7a47cd566
AlleyChang <yifanchang0602@gmail.com>
2022-07-27 16:11:07 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -4,26 +4,22 @@ const port = 3000
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')  //載入Mongoose
 const Todo = require('./models/todo')
+const bodyParser = require('body-parser')
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' })) //extname代表設定副檔名
 app.set('view engine', 'hbs')
 
-//取得資料庫連線狀態
-const db = mongoose.connection
-//設定連線至MongoDB
-mongoose.connect(process.env.MONGODB_URI) 
+app.use(bodyParser.urlencoded({ extended: true }))
 
- 
+const db = mongoose.connection   //取得資料庫連線狀態
+mongoose.connect(process.env.MONGODB_URI)    //設定連線至MongoDB
 
-// 連線異常，"on"=>監聽有無error事件發生
-db.on('error', () => {
+db.on('error', () => {    // 連線異常，"on"=>監聽有無error事件發生
   console.log('MongoDB error')
 })
-// 連線成功，once是一次性的監聽行為，監聽"open"有無連線成功，連線成功即解除監聽
-db.once('open', () => {
-  console.log('MongoDB connected!')
-  
-  
+
+db.once('open', () => {   // 連線成功，once是一次性的監聽行為，監聽"open"有無連線成功，連線成功即解除監聽
+  console.log('MongoDB connected!') 
 })
 
 app.get('/', (req, res) => {
@@ -33,6 +29,16 @@ app.get('/', (req, res) => {
       .catch(error => console.error(error))
 })
 
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name  //拿到表單內欲增加的資料
+  return Todo.create({name})  //存入資料庫
+    .then(() => res.redirect('/'))  //新增完成後重新導向回首頁
+    .catch(error => console.log(error))
+})
 app.listen(port, (req, res) => {
   console.log(`It is running on http://localhost:${port}.`)
 })
