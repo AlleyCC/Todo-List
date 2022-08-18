042396a8843ea7fc6c86340c236bb03ea4e012ae
AlleyChang <yifanchang0602@gmail.com>
2022-08-18 16:18:46 +0800

    feat: add user model

---
+++ models/user.js
@@ -0,0 +1,23 @@
+//資料庫綱要
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+//建構新的Schema
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+module.exports = mongoose.model('User', userSchema)
