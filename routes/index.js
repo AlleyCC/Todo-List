632d89abdf77af12aaee2e66db69ce7b3abaff87
AlleyChang <yifanchang0602@gmail.com>
2022-08-18 15:47:55 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const express = require('express')
 const router = express.Router()  //引用express的路由功能
 const home = require('./modules/home')
 const Todos = require('./modules/todos')
+const users = require('./modules/users')
 
 router.use('/', home)
 router.use('/todos', Todos)
+router.use('/users', users)
 
 module.exports = router  //匯出路由器
