2caf143a058df1a0d320cbeb17733d82de8ef933
AlleyChang <yifanchang0602@gmail.com>
2022-08-19 15:17:43 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes')  //引用路由器
 const session = require('express-session')
+const usePassport = require('./config/passport') //載入設定檔，必須寫在express-session之後
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' })) //extname代表設定副檔名
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+usePassport(app) //呼叫Passport並傳入app，必須寫在路由之前
 app.use(routes) //將request導入路由器
 app.use(session({
   secret: 'ThisIsMySecret',
